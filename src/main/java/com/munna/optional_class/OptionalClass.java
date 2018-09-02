3c5e9defbea44037c0eddb00cf0d51186935904b
Msyed <munna_syed@gap.com>
2018-09-02 22:54:00 +0530

    [Munna] Adding optional class

---
+++ src/main/java/com/munna/optional_class/OptionalClass.java
@@ -0,0 +1,22 @@
+package com.munna.optional_class;
+
+import com.munna.util.Employee;
+
+import java.util.Optional;
+
+public class OptionalClass {
+
+
+    public static void main(String[] args) {
+        String s = "AmmiAbba";
+    Employee employee = new Employee(26,"Munna");
+        //creating empty Optional
+        Optional<String> emptyOptional = Optional.empty();
+
+        //creating Optional using of()
+
+        Optional<Employee> stringOptional = Optional.of(employee);
+
+        System.out.println(stringOptional.get());
+    }
+}
